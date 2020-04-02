b9532e800087245b6c2e4ee3fa462aa613f5b3f7
BIOSYSTEM <46874046+lilbios@users.noreply.github.com>
2020-04-03 02:33:32 +0300

    added fonts

---
+++ TopGunLab_Practice3/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TopGunLab_Practice3.Models;
+
+namespace TopGunLab_Practice3.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        [HttpGet]
+        public ActionResult Index()
+        {
+            if (Session.Count == 0)
+            {
+                Session["Products"] = new List<Product>();
+            }
+            var products = Session["Products"] as List<Product>;
+            return View(products);
+        }
+    }
+}
