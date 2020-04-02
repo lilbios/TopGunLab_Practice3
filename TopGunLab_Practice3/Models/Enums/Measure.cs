1d102d8c05b4438e0c1cda4966f02c67ed64dabc
BIOSYSTEM <46874046+lilbios@users.noreply.github.com>
2020-04-02 23:07:39 +0300

    first commit

---
+++ TopGunLab_Practice3/Models/Enums/Measure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TopGunLab_Practice3.Models.Enums
+{
+    public enum Measure
+    {
+        Liter,
+        Gram,
+        Piece
+         
+    }
+}
---
+++ TopGunLab_Practice3/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using TopGunLab_Practice3.Models.Enums;
+
+namespace TopGunLab_Practice3.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public int Count { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime ProductionDate { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime ExcpiryDate { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        Measure Meausure { get; set; }
+        public string Logo { get; set; }
+    }
+}
